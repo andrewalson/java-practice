1f300923968c64515d755dc570027d751b24b4e4
andrewalson <83783605+andrewalson@users.noreply.github.com>
2023-10-05 19:39:10 -0400

    Add lengthOfLastWord.

---
+++ java-practice/src/lengthOfLastWord.java
@@ -0,0 +1,23 @@
+public class lengthOfLastWord {
+
+    public int getLengthOfLastWord(String s) {
+            
+        int stringLength = s.length();
+        int lastWordLength = 0;
+    
+            //loop over String backwards until you hit a non-space
+            //add +1 to lastWordLength counter
+            //keep looping until you hit (another) space, +1 to ^ w/ each before
+            //break and return int lastWordLength
+    
+        for (int i = stringLength - 1; i >= 0; i--) {
+            if (s.charAt(i)!=' ') {
+                lastWordLength++;
+            } else if (lastWordLength > 0) {
+                return lastWordLength;
+            }
+        }
+        return lastWordLength;
+    }
+}
+
