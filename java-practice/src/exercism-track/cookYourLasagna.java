84cd10f86403b3192710940394909ea3ace2f257
andrewalson <83783605+andrewalson@users.noreply.github.com>
2023-10-10 21:30:36 -0400

    Add Exercism track and cookYourLasagna.

---
+++ java-practice/src/exercism-track/cookYourLasagna.java
@@ -0,0 +1,22 @@
+public class cookYourLasagna {
+
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+    // ToDo: define the 'expectedMinutesInOven()' method
+
+    public int remainingMinutesInOven(int actualMinutesInOven) {
+        return (expectedMinutesInOven() - actualMinutesInOven);
+    }
+    // ToDo: define the 'remainingMinutesInOven()' method
+    
+    public int preparationTimeInMinutes(int numberOfLayers) {
+        return (numberOfLayers * 2);
+    }
+    // TODO: define the 'preparationTimeInMinutes()' method
+
+    public int totalTimeInMinutes(int numberOfLayers, int actualMinutesInOven) {
+        return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+    }
+    // TODO: define the 'totalTimeInMinutes()' method
+}
