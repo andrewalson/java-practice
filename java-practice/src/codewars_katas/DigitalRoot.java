afb4ae603691e056bf6381a8de9366786707d0f6
andrewalson <83783605+andrewalson@users.noreply.github.com>
2023-10-16 20:47:26 -0400

    CodeWars Sum of Digits / Digital Root.

---
+++ java-practice/src/codewars_katas/DigitalRoot.java
@@ -0,0 +1,20 @@
+package codewars_katas;
+
+public class DigitalRoot {
+    public static int digital_root(int n) {
+        if (n > 9) {
+            String string = String.valueOf(n);
+            String[] splitString = string.split("");
+
+            int sum = 0;
+            for (int i = 0; i < splitString.length; i++) {
+                sum = sum + Integer.parseInt(splitString[i]);
+            }
+
+            return digital_root(sum);
+        }
+        else {
+            return n;
+        }
+    }
+}
